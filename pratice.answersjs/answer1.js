26cf269c0c3280c0d128982626d49d6739b749a3
Bilal Khalid <bilal.khalid@systemsltd.com>
2021-11-08 10:28:48 +0500

    error fixed

--- pratice.answersjs/answer1.js
+++ pratice.answersjs/answer1.js
@@ -1,8 +1,8 @@
 // Q1 write a function which takes a number and prints its square hint : square is num*num
 // for example  2*2 = 4 so  4 is square of 2
-findsquare(num){
-let sq = num*num
-console.log("square of num is",sq)
+function findsquare(num) {
+    let sq = num * num
+    console.log("square of num is", sq)
 
 }
-findsquare (100)
+findsquare(100)
