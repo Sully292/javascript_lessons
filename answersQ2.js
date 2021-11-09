cbbff8e5492cdc70d08244d09b5f4d3e391317f0
Bilal Khalid <bilal.khalid@systemsltd.com>
2021-11-09 17:11:50 +0500

    question 2 fixed and q3 added

--- answersQ2.js
+++ answersQ2.js
@@ -3,8 +3,27 @@
 // exact number like 8 is smallest hint : use your code "a,b,c which one is smaller.js" 
 /// hint : to call your function you will do myfunc(55,85,8)
 
-function myfunc(smallest){
-let numb =  a = 75, b = 85 ,c = 90
-console.log(numb)
+// function myfunc(smallest) {
+//     let numb = a = 75, b = 85, c = 90
+//     console.log(numb)
+// }
+// myfunc(0)
+
+
+// your code was wrong i have made structure of answer complete it please
+
+
+function findSmallest(p, q, r) {
+    // complete remaining if condions
+    if (p < q && p < r) {
+        console.log(" the smallest number is ", p)
+    }
+
+
+
 }
-myfunc(0)
+
+// calling function
+
+findSmallest(2, 99, 5)
+
