31901c70946f5b30414325f13a462a10a280667b
unknown <sulemankhalid292@gmail.com>
2021-11-15 01:32:45 +0000

    maine kuch practice ki hai different scenerio laga ker please have a look

--- loops.js
+++ loops.js
@@ -1,5 +1,31 @@
-// while loop
+// while loop 
+// Function ko ek numb pass kren function utni dafa "hi " print kray
 
 // while(condition){
 
 // }
+'use strict'
+function p(a,b){
+    console.log(a,b)
+}
+function itnidafasalam(){
+  let x = 5
+    console.log("function is called",x)
+    while (x > 0){
+        console.log("slam",x,"slam kro")
+    x--
+    }
+
+}
+
+let myarray = ["apple","banana","mango","kiwi"]
+let x = myarray.length
+let count = 0
+while (count < x){
+    console.log(myarray[count])
+    count++
+}
+
+
+
+
---
+++ new.js
@@ -0,0 +1,17 @@
+
+function hellokrtyraho(a,b,c){
+    console.log(a,b,c)
+}
+///console.log mein function direct nai print kr rha tha ,it kept
+// coming back as 30 unidentified
+let w = 15, y = 14, z = 30
+while (y < w && z > w ){
+    console.log("hellokrtyrho")
+}
+
+let a =  4
+while (a % 2 == 0){
+    console.log("ager number even hai to btaty rho",a)
+}
+
+
