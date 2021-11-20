3e8d264ccdbd72e6d1a1c4936cc94359d06babb1
Bilal Khalid <bilal.khalid@systemsltd.com>
2021-11-20 18:26:58 +0500

    ... for loops

---
+++ loops/for_loops.js
@@ -0,0 +1,20 @@
+
+
+// for (let i = 0; i <= 7; i++) {
+//     console.log(i)
+// }
+
+
+let x = [73, 3838, 38, 728, 9]
+
+let len = x.length
+for (let i = 0; i < len; i++) {
+    console.log(x[i])
+}
+
+// printing array in reverse
+
+
+for (let i = len - 1; i >= 0; i = i - 1) {
+    console.log(x[i])
+}
