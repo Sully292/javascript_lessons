99f4e93f0febfa1a3bab893b167d749d2eab7ece
unknown <sulemankhalid292@gmail.com>
2021-11-20 13:13:02 +0000

    hanji

---
+++ pratice.answersjs/new2.js
@@ -0,0 +1,11 @@
+let myarray1 = [1,2,3,4,50,60]
+let x = myarray1.length
+
+// console.log(myarray1[x/2])
+let p = myarray1.length - 2
+// console.log(p)
+while (p >= 1){
+    console.log(myarray1[p])
+    p = p - 1
+}
+
