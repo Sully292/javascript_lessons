d12854bc342cc5d13cd2fbe05f80f33e13052923
unknown <sulemankhalid292@gmail.com>
2021-11-27 12:58:51 +0000

    g

--- number even or odd.js
+++ number even or odd.js
@@ -1,10 +1,10 @@
 //write a program which tells number is even or odd
 
-let a = 9 
-if ( a % 2 == 0){
-    console.log ("a is even")
+let a = 9
+if (a % 2 == 0) {
+    console.log("a is even")
 }
 
 else {
-    console.log ("number is odd")
+    console.log("number is odd")
 }
