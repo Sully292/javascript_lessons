a99211ab8475fd90dbe8ead4f2baf968af224e1d
Bilal Khalid <bilal.khalid@systemsltd.com>
2021-12-22 19:27:31 +0500

    arrays of object

---
+++ arrays/arrays_objects.js
@@ -0,0 +1,50 @@
+
+
+
+
+let myarray = [{
+    name: "Bilal",
+    age: 10,
+    education: "BA",
+    married: true,
+    skin: "brown"
+
+},
+{
+    name: "Ahmad",
+    age: 20,
+    education: "FA",
+    married: true,
+    skin: "black"
+
+}
+    , {
+    name: "suleman",
+    age: 30,
+    education: "FA",
+    married: false,
+    skin: "white"
+},
+{
+    name: "sadaf",
+    age: 25,
+    education: "FA",
+    married: true,
+    skin: "brown"
+}
+    ,
+{
+    name: "khalid",
+    age: 25,
+    education: "FA",
+    married: true,
+    skin: "brown"
+}
+]
+
+
+for (let i = 0; i < myarray.length; i++) {
+    console.log(myarray[i].name)
+}
+
+
