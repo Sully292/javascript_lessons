abd894e4a0d50faf46a8e50b09e54b4faaf7db21
unknown <sulemankhalid292@gmail.com>
2022-01-17 14:20:01 +0000

    object && or practice

--- object practice.js
+++ object practice.js
@@ -1,9 +1,37 @@
-let person = {
-    age:31, colour: "red",
-    kad: 6.3 , 
+let person = [{
+    name: "mushtaq",
+    age: 31, colour: "red",
+    kad: 6.3,
     school: "Government",
-    dob:"050791" ,
-    marks:21/25
- 
+    dob: "050791",
+    marks: 21 / 25
+
+},
+{
+    name: "jamil",
+    age: 45, colour: "blue",
+    kad: 6.9,
+    school: "private",
+    dob: "210885",
+    marks: 23 / 25
+},
+{
+    name: "bilal",
+    age: 65, colour: "green",
+    kad: 7,
+    school: "bahria",
+    dob: "150589",
+    marks: 18 / 25
+
 }
-console.log (person.kad)
+]
+// console.log(person.kad)
+for (let i = 0; i < person.length; i++) {
+    // console.log(person[i].age)
+}
+
+if (person.marks == 23 / 25 || person.name != "bilal" && person.age < 44) {
+    console.log(person.name, "yeh banda hai")
+
+}
+else { console.log("aisa koi banda nai") }
